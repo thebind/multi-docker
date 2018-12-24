3163325348e126e28bcd432a292d41c356948f6e
thebind <takeo@thebind.io>
2018-12-24 16:39:35 +0900

    9th commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
+
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Link, Route} from 'react-router-dom';
+
 import Fib from './Fib';
+import logo from './logo.svg';
+import OtherPage from './OtherPage';
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
+        <div className='App'>
+          <header className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h1 className='App-title'>Fib Calculator</h1>
+            <Link to='/'>Home</Link>
+            <Link to='/otherpage'>Other Page</Link>
           </header>
           <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
+            <Route exact path='/' component={
+      Fib} />
+            <Route path='/otherpage' component={OtherPage} />
           </div>
         </div>
       </Router>
